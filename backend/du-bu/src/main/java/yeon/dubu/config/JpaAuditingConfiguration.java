601045d943e9399fb796b98a296840ccbaed2dd2
ysparrk <youngseo0703@gmail.com>
2023-09-07 13:24:50 +0900

    feat: BaseTimeEntity 생성

---
+++ backend/du-bu/src/main/java/yeon/dubu/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package yeon.dubu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
