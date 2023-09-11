13ad206d07e2f9bcb987db2b38235ced620e356f
felix0942364 <jaewoongcho93@gmail.com>
2023-09-11 17:55:59 +0900

    feat: 검색 type 입력, 1000개 상품 가져오는걸로 수정

--- shopping_crawler.py
+++ shopping_crawler.py
@@ -11,10 +11,15 @@ from pathlib import Path
 load_dotenv()
 CLIENT_ID = os.environ.get('NAVER_ID')
 CLIENT_SECRET = os.environ.get('NAVER_PW')
-TYPES = ['냉장고', '침대']
-DISPLAY = 10
+# 가구 및 가전
+TYPES = ['소파', '침대', '책상', '식탁', '장롱', '화장대', '장식장', '의자', '서랍장', 
+         '냉장고', 'TV', '에어컨', '세탁기', '건조기', '전자레인지', '스타일러',
+         '로봇청소기', '청소기', '공기청정기', '선풍기', '에어프라이어', '밥솥']
+
+#총 1000개 상품 크롤링
 START = 1
-COUNT = 1
+COUNT = 10
+DISPLAY = 100 
 
 def get_shopping_data(client_id, client_secret, query, display, sort, start, count):
     url = "https://openapi.naver.com/v1/search/shop.json?query={}&display={}&sort={}&start={}".format(query, display, sort, start + count*100)
@@ -31,7 +36,7 @@ if __name__ == '__main__':
     for q in TYPES:
         q = urllib.parse.quote(q)
         for cnt in range(COUNT):
-            result += get_shopping_data(CLIENT_ID, CLIENT_SECRET, q, DISPLAY, 'sim', START, COUNT)
+            result += get_shopping_data(CLIENT_ID, CLIENT_SECRET, q, DISPLAY, 'sim', START, cnt)
 
     for idx, product in enumerate(result):
         img = ''.join(product['image'].split('\\'))
