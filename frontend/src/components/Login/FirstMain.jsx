2b498bf46aeedfbfec8d10e68adfbc419df11c8a
박승희 <tael8213@gmail.com>
2023-09-12 16:14:06 +0900

    feat: 소셜로그인 시도

--- frontend/src/components/Login/FirstMain.jsx
+++ frontend/src/components/Login/FirstMain.jsx
@@ -49,10 +49,18 @@ const KakaoImg = styled.img`
 `
 
 const FirstMain = () => {
+    const Rest_api_key= process.env.REACT_APP_REST_API_KEY //REST API KEY
+    const redirect_uri = `${process.env.REACT_APP_HOME_URL}`
+    //Redirect URI to backend
+    // oauth 요청 URL
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+    const handleLogin = ()=>{
+        window.location.href = kakaoURL
+    }
     return (
         <PageWrapper>
             <LoginTitle>연두부에서  <br/>스마트한 결혼 자금 모으기</LoginTitle>
-            <KakaoLogin><KakaoImg src={kakao} />카카오톡으로 시작하기</KakaoLogin>
+            <KakaoLogin onClick={handleLogin}><KakaoImg src={kakao} />카카오톡으로 시작하기</KakaoLogin>
         </PageWrapper>
     );
 };
