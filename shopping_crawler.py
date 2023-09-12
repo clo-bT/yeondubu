9ad4efeff37a6b49c427e2ceb2697e36c4fcdee5
felix0942364 <jaewoongcho93@gmail.com>
2023-09-12 15:58:37 +0900

    feat: 입출력 정리

--- shopping_crawler.py
+++ shopping_crawler.py
@@ -3,6 +3,7 @@ import cv2
 import urllib
 import numpy as np
 
+import json
 from dotenv import load_dotenv
 from PIL import Image
 from feature_extractor import FeatureExtractor
@@ -11,47 +12,56 @@ from pathlib import Path
 load_dotenv()
 CLIENT_ID = os.environ.get('NAVER_ID')
 CLIENT_SECRET = os.environ.get('NAVER_PW')
-# 가구 및 가전
-TYPES = ['소파', '침대', '책상', '식탁', '장롱', '화장대', '장식장', '의자', '서랍장', 
-         '냉장고', 'TV', '에어컨', '세탁기', '건조기', '전자레인지', '스타일러',
-         '로봇청소기', '청소기', '공기청정기', '선풍기', '에어프라이어', '밥솥']
+
+# with open('furniture.json', 'r') as file1:
+#     furniture = json.load(file1)
+# with open('appliances.json', 'r') as file2:
+#     appliances = json.load(file2)
+# queries = {**furniture, **appliances}
+
+queries = {"sofa":"소파", "desk":"책상", "drawer":"서랍장"}
 
 #총 1000개 상품 크롤링
+# START = 1
+# COUNT = 10
+# DISPLAY = 100
 START = 1
-COUNT = 10
-DISPLAY = 100 
+COUNT = 1
+DISPLAY = 10
 
 def get_shopping_data(client_id, client_secret, query, display, sort, start, count):
-    url = "https://openapi.naver.com/v1/search/shop.json?query={}&display={}&sort={}&start={}".format(query, display, sort, start + count*100)
+    url = "https://openapi.naver.com/v1/search/shop.json?query={}&display={}&sort={}&start={}".format(query, display, sort, start + count*display)
     request = urllib.request.Request(url)
     request.add_header('X-Naver-Client-Id', client_id)
     request.add_header('X-Naver-Client-Secret', client_secret)
+
     response = urllib.request.urlopen(request)
     dic = response.read().decode('utf-8')
     return eval(dic)['items']
 
 if __name__ == '__main__':
     fe = FeatureExtractor()
-    result = []
-    for q in TYPES:
-        q = urllib.parse.quote(q)
+    for cat, query in queries.items():
+        result = []
+        query = urllib.parse.quote(query)
         for cnt in range(COUNT):
-            result += get_shopping_data(CLIENT_ID, CLIENT_SECRET, q, DISPLAY, 'sim', START, cnt)
+            result += get_shopping_data(CLIENT_ID, CLIENT_SECRET, query, DISPLAY, 'sim', START, cnt)
 
-    for idx, product in enumerate(result):
-        img = ''.join(product['image'].split('\\'))
-        res = urllib.request.urlopen(img).read()
-        image_np = np.frombuffer(res, dtype=np.uint8)
-        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-        cv2.imwrite('./tmp/' + str(idx) + '.png', image)
-        '''
-        BGrem 이 부분에서 식별해서 배경 제거할것
-        OS.remove(원본파일)
-        '''
-        feature = fe.extract(img=Image.open('./tmp/' + str(idx) + '.png'))
-        feature_path = Path("./static/feature") / (str(idx) + ".npy")
-        '''
-        OS.remove(배경제거파일)
-        '''
-        np.save(feature_path, feature)
-        # os.remove('./tmp/tmp.png')
+        features = []
+        features_path = Path("./static/feature") / (str(cat) + ".npy")
+        for idx, product in enumerate(result):
+            img = ''.join(product['image'].split('\\'))
+            res = urllib.request.urlopen(img).read()
+            image_np = np.frombuffer(res, dtype=np.uint8)
+            image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+            cv2.imwrite('./tmp/' + str(idx) + '.png', image)
+            '''
+            BGrem 이 부분에서 식별해서 배경 제거할것
+            OS.remove(원본파일)
+            '''
+            feature = fe.extract(img=Image.open('./tmp/' + str(idx) + '.png'))
+            features.append(feature)
+            '''
+            OS.remove(배경제거파일)
+            '''
+        np.save(features_path, np.array(features))
