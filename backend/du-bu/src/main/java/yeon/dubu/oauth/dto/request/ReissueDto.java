4499bc90e33e76437c17a8d9ccd0581a03a36939
ysparrk <youngseo0703@gmail.com>
2023-09-13 10:54:19 +0900

    remove: 소셜로그인 구현 중 안쓰는 파일 삭제

--- backend/du-bu/src/main/java/yeon/dubu/oauth/dto/request/ReissueDto.java
+++
@@ -1,8 +0,0 @@
-package yeon.dubu.oauth.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class ReissueDto {
-    private String refreshToken;
-}
--- backend/du-bu/src/main/java/yeon/dubu/oauth/service/AuthUserService.java
+++
@@ -1,9 +0,0 @@
-package yeon.dubu.oauth.service;
-
-import yeon.dubu.oauth.dto.request.SignInDto;
-import yeon.dubu.oauth.dto.response.SignInResponseDto;
-
-public interface AuthUserService {
-    SignInResponseDto signIn(SignInDto signInDto) throws Exception;
-
-}
