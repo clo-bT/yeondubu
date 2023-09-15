992e8ea51942a40e500f2ddbcc715f15d93b4993
박승희 <pshee0329@naver.com>
2023-09-15 16:52:11 +0900

    feat: calendar modal 형태 완성, 에러 고침

--- frontend/src/components/CalendarInputCreate/CalendarInput.jsx
+++ frontend/src/components/CalendarInputCreate/CalendarInput.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CalendarInputPage = styled.div``
+const CalendarInputPage = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  `
+
+const ModalContent = styled.div`
+  background: #fff;
+  border-radius: 10px;
+  padding: 20px;
+  width: 400px;
+`
 const InputType = styled.div``
-const Button = styled.button``
+const CloseButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 20px;
+`
 const InputWho = styled.div``
-const InputWhoGroom = styled.div`
+const InputWhoGroom = styled.button`
     width: 87px;
     height: 38px;
     padding: 6px 4px;
@@ -14,7 +37,7 @@ const InputWhoGroom = styled.div`
     border: 2px solid #D9D9D9;
     background: #FFF;
 `
-const InputWhoBride = styled.div`
+const InputWhoBride = styled.button`
     width: 87px;
     height: 38px;
     padding: 6px 4px;
@@ -34,29 +57,39 @@ const CloseModal = styled.div``
 
 // const TagCreatePage = styled.div``
 const CalendarInput = ({ isOpen, content, formatday, closeModal }) => {
+
     if (!isOpen) {
         return null; // 모달이 닫혀있으면 아무것도 렌더링하지 않음
-      }
-    return (
-        <CalendarInputPage>
-            <CloseModal onClick={closeModal}><Button>X</Button></CloseModal>
-            <InputType>
-                <InputSpan>분류 </InputSpan>
-            </InputType>
-            
-            <InputWho><InputSpan>결제자 </InputSpan>
-                <InputWhoGroom>
-                    예비신랑
-                </InputWhoGroom>
-                <InputWhoBride>
-                    예비신부
-                </InputWhoBride>
-            </InputWho>
+    }
+    const handleModalClick = (e) => {
+        e.stopPropagation(); // 이벤트 버블링 중단
+    };
 
-            <InputTag><InputSpan>태그 </InputSpan></InputTag>
-            <InputDate><InputSpan>날짜 </InputSpan> {formatday}</InputDate>
-            <InputMoney><InputSpan>가격 </InputSpan> {content.amount.toLocaleString()} 원</InputMoney>
-            <InputMemo><InputSpan>메모 </InputSpan></InputMemo>
+
+    return (
+        <CalendarInputPage onClick={closeModal}>
+            <ModalContent onClick={handleModalClick}>
+                <CloseModal onClick={closeModal}><CloseButton>X</CloseButton></CloseModal>
+                <InputType>
+                    <InputSpan>분류 </InputSpan>
+                </InputType>
+                
+                <InputWho><InputSpan>결제자 </InputSpan>
+                    <InputWhoGroom>
+                        예비신랑
+                    </InputWhoGroom>
+                    <InputWhoBride>
+                        예비신부
+                    </InputWhoBride>
+                </InputWho>
+
+                <InputTag><InputSpan>태그 </InputSpan></InputTag>
+                <InputDate><InputSpan>날짜 </InputSpan> {formatday}</InputDate>
+                <InputMoney><InputSpan>가격 </InputSpan> {content.amount.toLocaleString()} 원</InputMoney>
+                <InputMemo><InputSpan>메모 </InputSpan></InputMemo>
+
+
+            </ModalContent>
         </CalendarInputPage>
     );
 };
