113cffd2fa58a8238692c44859638fb7b8f4b014
김재헌 <tint1998@gmail.com>
2023-09-18 13:20:20 +0900

    feat : couple connection

---
+++ backend/du-bu/src/main/java/yeon/dubu/couple/dto/response/CoupleCodeCreateRes.java
@@ -0,0 +1,11 @@
+package yeon.dubu.couple.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CoupleCodeCreateRes {
+    private String state;
+    private Long guestId;
+}
---
+++ backend/du-bu/src/main/java/yeon/dubu/couple/repository/CoupleConnectionRepository.java
@@ -0,0 +1,12 @@
+package yeon.dubu.couple.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import yeon.dubu.couple.domain.CoupleConnection;
+
+@Repository
+public interface CoupleConnectionRepository extends JpaRepository<CoupleConnection, Long> {
+    Optional<CoupleConnection> findByUserId(Long userId);
+
+}
