77a562f464ce49a232ad4f12d55a410076e60bf9
seongwonlee <jasmine1714@naver.com>
2023-09-18 17:18:04 +0900

    feat : shoppingMall, putshoppingimage 페이지 구현

--- frontend/src/pages/ShoppingMall.jsx
+++ frontend/src/pages/ShoppingMall.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ShopCategoryHeader from '../components/ShoppingMall/ShopCategoryHeader';
 import ShoppingList from '../components/ShoppingMall/ShoppingList';
+import Menubar from '../components/Common/Menubar';
 
 const ShoppingMall = () => {
     return (
         <div>
             <ShopCategoryHeader />
             <ShoppingList />
+            <Menubar />
         </div>
     );
 };
