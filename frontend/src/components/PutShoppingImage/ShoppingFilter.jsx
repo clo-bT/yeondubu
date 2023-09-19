f6b2167f1c7e6acc707d5113666e89801e4304d1
seongwonlee <jasmine1714@naver.com>
2023-09-19 14:30:12 +0900

    feat: productrecommend 페이지 구현 완료

--- frontend/src/components/PutShoppingImage/ShoppingFilter.jsx
+++ frontend/src/components/PutShoppingImage/ShoppingFilter.jsx
@@ -1,7 +1,6 @@
 import {React, useState} from 'react';
 import styled from 'styled-components';
 import PictureInput from '../../assets/PutShoppingImage/PictureInput.svg';
-import { AiFillPlusCircle } from "react-icons/ai";
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
@@ -10,7 +9,8 @@ overflow-y: auto;
 margin-left: 20px;
 margin-right: 20px;
 margin-top: 20px;
-margin-bottom: 30px;
+margin-bottom: 110px;
+height: auto;
 `
 const GetOutButton = styled.a`
 color: #000;
@@ -39,7 +39,6 @@ margin-bottom: 30px;
 const Box1 = styled.div`
 display: flex;
 justify-content : space-between ;
-/* margin-bottom: 20px; */
 
 `
 const InputPicture = styled.img`
@@ -59,12 +58,7 @@ font-style: normal;
 font-weight: 700;
 line-height: normal;    
 `
-const PlusIcon = styled(AiFillPlusCircle)`
-margin-left: 5px;
-fill: rgba(255, 147, 125, 0.30);
-color: #000;
 
-`
 
 const PriceSliderContainer = styled.div`
   padding: 20px;
@@ -81,16 +75,56 @@ font-weight: 400;
 line-height: normal;
 `
 
+const Brand = styled.p`
+display: flex;
+font-size: 10px;
+height: 5px;
+width: 10px;
+padding: 10px 18px;
+justify-content: center;
+align-items: center;
+gap: 10px;
+border-radius: 10px;
+border: 1px solid #FF6565;
+margin-top: 0px;
+margin-bottom: 0px;
+
+/* 배경색을 brandClickStates에 따라 조절 */
+background-color: ${(props) =>
+  props.isClicked ? '#FF5959' : 'transparent'};
+color: ${(props) => (props.isClicked ? '#fff' : 'rgba(0, 0, 0, 0.80)')};
+cursor: pointer;
+`
+
+const BrandBox = styled.div`
+display: flex;
+gap : 10px;
+flex-wrap: wrap;
+
+`
+
 const ShoppingFilter = () => {
+  const brands = ['LG', 'Samsung', 'Apple', 'Sony', 
+  'LG1', 'Samsung1', 'Apple1', 'Sony1',
+];
+
+
 
     const [selectedImage, setSelectedImage] = useState(null);
     const [priceRange, setPriceRange] = useState([0, 1000]);
     const [currentPriceRange, setCurrentPriceRange] = useState(priceRange); // 현재 구간 값을 저장할 상태
-  const handleSliderChange = (value) => {
-    setPriceRange(value);
-    const scrollPosition = (value[0] / 1000) * document.documentElement.scrollHeight;
-    window.scrollTo(0, scrollPosition);
-  };
+    
+    // 브랜드 클릭 상태를 관리하는 배열
+    const [brandClickStates, setBrandClickStates] = useState(
+      new Array(brands.length).fill(false)
+    );
+      
+    
+    const handleSliderChange = (value) => {
+      setPriceRange(value);
+      const scrollPosition = (value[0] / 1000) * document.documentElement.scrollHeight;
+      window.scrollTo(0, scrollPosition);
+    };
     // 이미지를 선택했을 때 처리하는 함수
     const handleImageSelect = (event) => {
     const file = event.target.files[0]; // 선택한 파일
@@ -106,6 +140,26 @@ const ShoppingFilter = () => {
         setCurrentPriceRange(value);
         console.log('슬라이더 구간 값:', value);
     };
+
+    // 클릭한 브랜드의 클릭 상태를 토글 
+    const handleBrandClick = (index) => {
+      const updatedClickStates = [...brandClickStates];
+      updatedClickStates[index] = !updatedClickStates[index];
+      setBrandClickStates(updatedClickStates);
+      
+      // 클릭한 브랜드 정보 배열 업데이트
+      const clickedBrands = [];
+      updatedClickStates.forEach((clicked, index) => {
+        if (clicked) {
+          clickedBrands.push(brands[index]);
+        }
+      });
+
+      // 클릭한 브랜드 배열 출력
+      console.log('클릭한 브랜드들:', clickedBrands);
+    };
+
+
     return (
         <Container>
             <Box>
@@ -128,11 +182,22 @@ const ShoppingFilter = () => {
           onChange={handleImageSelect} 
         />
         <Box1>
-        <Header>브랜드<PlusIcon /></Header>
+        <Header>브랜드</Header>
         </Box1>
+
+        <BrandBox>
+        {brands.map((brand, index) => (
+          <Brand
+          key={brand}
+          isClicked={brandClickStates[index]}
+          onClick={() => handleBrandClick(index)}
+          >{brand}</Brand>
+        ))}
+        </BrandBox>
 
         <Box1>
         <Header>가격대 설정</Header>
+
         <PriceRange>
           {currentPriceRange[0].toLocaleString()} - {currentPriceRange[1].toLocaleString()}원
         </PriceRange>
