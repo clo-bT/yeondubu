9df7ef314f61c965c56ff2fc77dd2482578db85a
ysparrk <youngseo0703@gmail.com>
2023-09-20 17:52:52 +0900

    feat: 사용자의 지출 등록 기능 구현

--- backend/du-bu/src/main/java/yeon/dubu/money/controller/MoneyExpenditureController.java
+++ backend/du-bu/src/main/java/yeon/dubu/money/controller/MoneyExpenditureController.java
@@ -1,4 +1,39 @@
 package yeon.dubu.money.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import yeon.dubu.money.domain.MoneyExpenditure;
+import yeon.dubu.money.dto.request.MoneyExpenditureReqDto;
+import yeon.dubu.money.service.MoneyExpenditureService;
+
+import java.net.URISyntaxException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/money")
 public class MoneyExpenditureController {
+
+    private MoneyExpenditureService moneyExpenditureService;
+
+    /**
+     * couple의 예산안 or 지출 등록
+     * @param userId
+     * @param moneyExpenditureReqDto
+     * @return
+     * @throws URISyntaxException
+     */
+    @PostMapping("/expenditure")
+    public ResponseEntity insertExpenditure(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody MoneyExpenditureReqDto moneyExpenditureReqDto
+            ) throws URISyntaxException {
+
+        MoneyExpenditure moneyExpenditure = moneyExpenditureService.insertExpenditure(moneyExpenditureReqDto, userId);
+
+        return ResponseEntity.ok(moneyExpenditure);
+    }
 }
--- backend/du-bu/src/main/java/yeon/dubu/money/service/MoneyExpenditureServiceImpl.java
+++ backend/du-bu/src/main/java/yeon/dubu/money/service/MoneyExpenditureServiceImpl.java
@@ -5,16 +5,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import yeon.dubu.category.domain.TagExpenditure;
 import yeon.dubu.category.repository.TagExpenditureRepository;
 import yeon.dubu.couple.domain.Couple;
 import yeon.dubu.couple.exception.NoSuchCoupleException;
 import yeon.dubu.couple.repository.CoupleRepository;
 import yeon.dubu.money.domain.MoneyExpenditure;
 import yeon.dubu.money.dto.request.MoneyExpenditureReqDto;
+import yeon.dubu.money.repository.MoneyExpenditureRepository;
 import yeon.dubu.money.repository.MoneyRepository;
 import yeon.dubu.user.domain.User;
 import yeon.dubu.user.exception.NoSuchUserException;
 import yeon.dubu.user.repository.UserRepository;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -25,6 +29,7 @@ public class MoneyExpenditureServiceImpl implements MoneyExpenditureService{
     private final UserRepository userRepository;
     private final CoupleRepository coupleRepository;
     private final MoneyRepository moneyRepository;
+    private final MoneyExpenditureRepository moneyExpenditureRepository;
     private final TagExpenditureRepository tagExpenditureRepository;
 
 
@@ -40,10 +45,19 @@ public class MoneyExpenditureServiceImpl implements MoneyExpenditureService{
         User user = userRepository.findById(userId).orElseThrow(() -> new NoSuchUserException("해당하는 회원 정보가 없습니다."));
         Couple couple = coupleRepository.findById(user.getCouple().getId()).orElseThrow(() -> new NoSuchCoupleException("해당하는 커플 정보가 없습니다."));
 
-        // 태그 조회
+        // 태그 조회 -> 태그 조회해서 거기에 넣기
+        Optional<TagExpenditure> searchTags = tagExpenditureRepository.findTagExpenditureByCoupleAndFirstTagNameAndSecondTagNameAndThirdTagName(couple, moneyExpenditureReqDto.getFirstTagName(), moneyExpenditureReqDto.getSecondTagName(), moneyExpenditureReqDto.getThirdTagName());
 
+        MoneyExpenditure moneyExpenditure = MoneyExpenditure.builder()
+                .userRole(moneyExpenditureReqDto.getUserRole())
+                .date(moneyExpenditureReqDto.getDate())
+                .amount(moneyExpenditureReqDto.getAmount())
+                .memo(moneyExpenditureReqDto.getMemo())
+                .tagExpenditure(searchTags.get())
+                .build();
 
+        moneyExpenditureRepository.save(moneyExpenditure);
 
-        return null;
+        return moneyExpenditure;
     }
 }
