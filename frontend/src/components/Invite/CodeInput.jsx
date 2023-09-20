22583b25b30419a033172d67640c36d6e436fbdd
박승희 <pshee0329@naver.com>
2023-09-20 18:00:53 +0900

    feat: 로그인, 코드생성 페이지

---
+++ frontend/src/components/Invite/CodeInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Container = styled.div`
+    
+`
+const Header = styled.div`
+    
+`
+
+
+const CodeInput = () => {
+    
+    return (
+        <Container>
+            <Header>
+                약혼자의 코드를 입력해주세요
+            </Header>
+        </Container>
+    );
+};
+
+export default CodeInput;
