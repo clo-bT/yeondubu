805ec64b82361880624ba2238f7c35f5920a5b70
ysparrk <youngseo0703@gmail.com>
2023-09-21 15:32:41 +0900

    fix: fix merge conflicts

--- backend/du-bu/src/main/java/yeon/dubu/expenditure/repository/TagExpenditureRepository.java
+++ backend/du-bu/src/main/java/yeon/dubu/expenditure/repository/TagExpenditureRepository.java
@@ -6,6 +6,7 @@ import yeon.dubu.couple.domain.Couple;
 import java.util.Optional;
 import yeon.dubu.expenditure.domain.TagExpenditure;
 
+
 public interface TagExpenditureRepository extends JpaRepository<TagExpenditure, Long> {
     Optional<TagExpenditure> findTagExpenditureByCoupleAndFirstTagNameAndSecondTagNameAndThirdTagName(Couple couple, String firstTagName, String secondTagName, String thirdTagName);
 }
