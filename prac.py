3dfd5b22d8fdff9b005baac59f5c193bf3536236
YuJinUk <wlsdnrzlzl@naver.com>
2023-09-21 09:44:15 +0900

    feat: add loans data

--- prac.py
+++ prac.py
@@ -1,70 +1,33 @@
-# import sys
-# input = sys.stdin.readline
-
-# def quick_sort(arr):
-#     if len(arr) <= 1:
-#         return arr
-#     pivot = arr[len(arr) // 2]
-#     lesser_arr, equal_arr, greater_arr = [], [], []
-#     for num in arr:
-#         if num < pivot:
-#             lesser_arr.append(num)
-#         elif num > pivot:
-#             greater_arr.append(num)
-#         else:
-#             equal_arr.append(num)
-#     return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
-
-# while True:
-#     try:
-#         x = int(input()) * 10000000
-#         N = int(input())
-#         check = [int(input()) for _ in range(N)]
-#         check = quick_sort(check)
-#         # 10 cm = 100,000,000 nm
-#         left, right, f = 0, N-1, True
-#         while left < right:
-#             mid = (left + right) //2
-#             if check[left] + check[right] == x:
-#                 print(f'yes {check[left]} {check[right]}')
-#                 f = False
-#                 break
-#             elif check[left] + check[right] < x:
-#                 left = mid + 1
-#             else:
-#                 right = mid - 1
-#         if f: print('danger')
-#     except:
-#         sys.exit(0)
-        
-        
-import sys
-input=sys.stdin.readline
-
-while True:
-    try:
-        x = int(input())
-        N = int(input())
-        check = sorted([int(input()) for _ in range(N)])
-
-        left = 0 ; right = N-1 ; answer = -1 ; answer_list = []
-
-        while left < right:
-            if check[left] + check[right] == x * 10000000:
-
-                if answer < abs(check[right]-check[left]):
-                    answer_list = [check[left], check[right]]
-                    answer = abs(check[right]-check[left])
-                left += 1
-
-            elif check[left] + check[right] > x * 10000000:
-                right -= 1
-            else:
-                left += 1
-
-        if len(answer_list):
-            print("yes %d %d"%(answer_list[0] , answer_list[1]))
-        else:
-            print('danger')
-    except:
-        break
+def credit_scores(score, money):
+    import json
+    import heapq
+    with open('./personal_credit_loans.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    change_to_en = {
+        1 : 'bottom',
+        2 : 'second',
+        3 : 'third',
+        4 : 'fourth',
+        5 : 'fifth',
+        6 : 'sixth',
+        7 : 'seventh',
+        8 : 'eighth',
+    }
+    if score <= 300:
+        score = 8
+    else:
+        score = 10 - (score - 1)//100
+    en_score = change_to_en[score]
+    minheap, result = [], []
+    for idx in data:
+        rate = data[idx][f'rate_{en_score}']
+        if type(rate) == float:
+            heapq.heappush(minheap, (rate, idx))
+    while minheap:
+        x = heapq.heappop(minheap)
+        da = data[x[1]]
+        interest = money * da[f'rate_{en_score}']
+        da['interest'] = interest
+        result.append(da)
+    return result[:10]
+print(credit_scores(803, 50000000))
