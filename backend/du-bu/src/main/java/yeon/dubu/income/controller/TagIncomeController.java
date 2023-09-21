766267c8cc3c4b673c83aeaab1fa940589140f77
김재헌 <tint1998@gmail.com>
2023-09-21 17:43:26 +0900

    feat: income-api 개발

--- backend/du-bu/src/main/java/yeon/dubu/income/controller/TagIncomeController.java
+++ backend/du-bu/src/main/java/yeon/dubu/income/controller/TagIncomeController.java
@@ -1,14 +1,34 @@
 package yeon.dubu.income.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yeon.dubu.income.domain.TagIncome;
+import yeon.dubu.income.dto.response.TagIncomeResDto;
+import yeon.dubu.income.service.TagIncomeService;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/accounts")
+@RequestMapping("/api/v1/income/tag")
 @RequiredArgsConstructor
 public class TagIncomeController {
+    private final TagIncomeService tagIncomeService;
+    @PostMapping("/{tagName}")
+    public ResponseEntity<?> insertTag(@PathVariable String tagName){
+        tagIncomeService.insertTag(tagName);
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
 
+    @GetMapping
+    public ResponseEntity<?> searchTag(){
+        List<TagIncomeResDto> tagIncomeResDtoList = tagIncomeService.searchTag();
+        return new ResponseEntity<>(tagIncomeResDtoList, HttpStatus.OK);
+    }
 }
