501e2f620724c6f6e6fbbbf153954c9827eca9f7
seongwonlee <jasmine1714@naver.com>
2023-09-21 15:55:20 +0900

    feat: 사용자 정보 받기

---
+++ frontend/src/pages/CreditScoreInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ScoreInputHeader from '../components/CreditScoreInput/ScoreInputHeader';
+import ScoreInput from '../components/CreditScoreInput/ScoreInput';
+
+const CreditScoreInput = () => {
+    return (
+        <div>
+            <ScoreInputHeader/>
+            <ScoreInput />
+        </div>
+    );
+};
+
+export default CreditScoreInput;
--- frontend/src/pages/WeddingDayUpdate.jsx
+++ frontend/src/pages/WeddingDayUpdate.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import WeddingDayUpdateInput from '../components/WeddingDayUpdate/WeddingDayUpdateInput';
+import CoupleImage from '../components/Common/CoupleImage';
 
 const WeddingDayUpdate = () => {
     return (
         <div>
+            <CoupleImage />
             <WeddingDayUpdateInput />
         </div>
     );
