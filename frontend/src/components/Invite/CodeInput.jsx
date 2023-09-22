69461b5282cdac6e2c88fb6acdec5b9e032a3de1
박승희 <tael8213@gmail.com>
2023-09-22 17:18:11 +0900

    feat: 상대 확인 페이지 생성

--- frontend/src/components/Invite/CodeInput.jsx
+++ frontend/src/components/Invite/CodeInput.jsx
@@ -4,23 +4,34 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
-    
+    background-color: #FFD0D0;
+    height: calc(var(--vh, 1vh) * 100);
+    width: 100%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const CodeInputBox = styled.div`
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
 `;
 const Header = styled.div`
-    
+    display: flex;
+    width: 312px;
+    height: 58px;
+    flex-direction: column;
+    justify-content: center;
 `;
 const InputContainer = styled.div`
     display: flex;
     justify-content: center;
+    width: 293px;
+    height: 47px;
 `;
-const CreateCode = styled.div`
-    cursor: pointer;
-`
-const InputButton = styled.div`
-    cursor: pointer;
-
-`
-
 const NumberInput = styled.input`
 
     width: 30px;
@@ -29,6 +40,31 @@ const NumberInput = styled.input`
     margin: 0 10px;
 
 `;
+const InputButton = styled.div`
+    width: 197px;
+    height: 64px;
+    border-radius: 15px;
+    background: #F5F5F5;
+    color: #FF5A5A;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    cursor: pointer;
+
+`;
+const CreateCode = styled.div`
+    color: #FFF;
+    text-align: center;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    text-decoration-line: underline;
+    cursor: pointer;
+`;
+
 
 const CodeInput = () => {
     const navigate = useNavigate();
@@ -73,7 +109,10 @@ const CodeInput = () => {
         })
             .then((response) => {
                 // 요청 성공 시 처리
-                console.log('요청 성공:', response);
+                console.log('요청 성공:', response.data.state);
+                if (response.data.state === 'success') {
+                    navigate(`/check`)
+                };
             })
             .catch((error) => {
                 // 요청 실패 시 처리
@@ -84,22 +123,23 @@ const CodeInput = () => {
 
     return (
         <Container>
-            <Header>
-                약혼자의 코드를 입력해주세요
-            </Header>
-            <InputContainer>
-                {inputRefs.map((ref, index) => (
-                    <NumberInput
-                        key={index}
-                        type="text"
-                        ref={ref}
-                        onChange={(e) => handleInputChange(e, index)}
-                    />
-                ))}
-            </InputContainer>
-            <InputButton onClick={sendCodeToBackend}>코드 입력</InputButton>
-            <CreateCode onClick={() => navigate("/invite")}>코드 생성</CreateCode>
-            
+            <CodeInputBox>
+                <Header>
+                    약혼자의 코드를 입력해주세요
+                </Header>
+                <InputContainer>
+                    {inputRefs.map((ref, index) => (
+                        <NumberInput
+                            key={index}
+                            type="text"
+                            ref={ref}
+                            onChange={(e) => handleInputChange(e, index)}
+                        />
+                    ))}
+                </InputContainer>
+                <InputButton onClick={sendCodeToBackend}>코드 입력</InputButton>
+                <CreateCode onClick={() => navigate("/invite")}>코드 생성</CreateCode>
+            </CodeInputBox>
         </Container>
     );
 };
