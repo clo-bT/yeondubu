0fa5fc177468968321afbfd05cb17a1151043919
seongwonlee <jasmine1714@naver.com>
2023-09-22 15:54:22 +0900

    feat: checkbox page

--- frontend/src/components/CheckBox/CheckListUpdate.jsx
+++ frontend/src/components/CheckBox/CheckListUpdate.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styled from 'styled-components';
 import { AiFillMinusCircle } from "react-icons/ai";
 
@@ -29,7 +29,6 @@ line-height: normal;
 const ThirdTag = styled.p`
 color: #000;
 text-align: left;
-font-family: Inter;
 font-size: 16px;
 font-style: normal;
 font-weight: 400;
@@ -41,130 +40,114 @@ const ThirdTagContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+ 
 `
 const MinusIcon = styled(AiFillMinusCircle)`
 color: #FF937D4D;
 margin-right: 5px;
+cursor: pointer;
 `
 
-const HorizonLine = styled.p`
-background: #FFD0D0;    
-margin-top: 35px;
-width: 100%;
-height: 1.5px;
-`
 
 const CheckListUpdate = () => {
-    const dummyData = [
-        {
-          index: 0,
-          firstTag: '혼수',
-          secondTag: '가구',
-          thirdTag: [
-            '침대',
-            '장롱',
-            '쇼파',
-            '식탁',
-          ],
-        },
-        {
-          index: 1,
-          firstTag: '혼수',
-          secondTag: '가전',
-          thirdTag: [
-            'TV',
-            '세탁기',
-            '청소기',
-            '냉장고',
-          ],
-        },
-        {
-          index: 2,
-          firstTag: '혼수',
-          secondTag: '기타',
-          thirdTag: [
-            '냄비',
-            '수저',
-            '그릇',
-            '도마',
-          ],
-        },
-        {
-          index: 3,
-          firstTag: '결혼식',
-          secondTag: '스튜디오',
-          thirdTag: [
-            '냄비',
-            '수저',
-            '그릇',
-            '도마',
-          ],
-        },
-        {
-          index: 4,
-          firstTag: '결혼식',
-          secondTag: '드레스',
-          thirdTag: [
-            '드레스',
-            '한복',
-          ],
-        },
-        {
-          index: 5,
-          firstTag: '결혼식',
-          secondTag: '메이크업',
-          thirdTag: [
-            '혼주 메이크업',
+  const [dummyData, setDummyData] = useState([
+    {
+      index: 0,
+      firstTag: '혼수',
+      secondTag: '가구',
+      thirdTag: [
+        '침대',
+        '장롱',
+        '쇼파',
+        '식탁',
+      ],
+    },
+    {
+      index: 1,
+      firstTag: '혼수',
+      secondTag: '가전',
+      thirdTag: [
+        'TV',
+        '세탁기',
+        '청소기',
+        '냉장고',
+      ],
+    },
+    {
+      index: 2,
+      firstTag: '혼수',
+      secondTag: '기타',
+      thirdTag: [
+        '냄비',
+        '수저',
+        '그릇',
+        '도마',
+      ],
+    },
+    {
+      index: 3,
+      firstTag: '결혼식',
+      secondTag: '스튜디오',
+      thirdTag: [
+        '냄비',
+        '수저',
+        '그릇',
+        '도마',
+      ],
+    },
+    {
+      index: 4,
+      firstTag: '결혼식',
+      secondTag: '드레스',
+      thirdTag: [
+        '드레스',
+        '한복',
+      ],
+    },
+    {
+      index: 5,
+      firstTag: '결혼식',
+      secondTag: '메이크업',
+      thirdTag: [
+        '혼주 메이크업',
+      ],
+    },
+    {
+      index: 6,
+      firstTag: '결혼식',
+      secondTag: '예식장',
+      thirdTag: [
+        '웨딩홀 대관',
+        '사회',
+        '축가',
+      ],
+    },
+  ]);
 
-          ],
-        },
-        {
-          index: 6,
-          firstTag: '결혼식',
-          secondTag: '예식장',
-          thirdTag: [
-            '웨딩홀 대관',
-            '사회',
-            '축가',
-          ],
-        },
-      ];
       
-      let currentFirstTag = ''; // 현재의 firstTag 값을 저장하는 변수
 
-      return (
-        <Container>
-          {dummyData.map((item) => {
-            if (item.firstTag !== currentFirstTag) {
-              currentFirstTag = item.firstTag; // 현재의 firstTag 값을 업데이트
-              return (
-                <div key={item.index}>
-                  <FirstTag>{item.firstTag}</FirstTag>
-                  <SecondTag>{item.secondTag}</SecondTag>
-                  <ThirdTagContainer>
-                  
-                  {item.thirdTag.map((thirdItem, index) => (
-                    <ThirdTag key={index}>< MinusIcon /> {thirdItem}</ThirdTag>
-                  ))}
+  const removeThirdTag = (firstTagIndex, thirdTagIndex) => {
+    const updatedDummyData = [...dummyData];
+    updatedDummyData[firstTagIndex].thirdTag.splice(thirdTagIndex, 1);
+    setDummyData(updatedDummyData);
+  };
 
-                  </ThirdTagContainer>
-                </div>
-              );
-            } else {
-              return (
-                <div key={item.index}>
-                  <SecondTag>{item.secondTag}</SecondTag>
-                  <ThirdTagContainer>
-                    {item.thirdTag.map((thirdItem, index) => (
-                    <ThirdTag key={index}>< MinusIcon />{thirdItem}</ThirdTag>
-                  ))}  
-                  </ThirdTagContainer>
-                </div>
-              );
-            }
-        })}
-
-        </Container>
+  return (
+    <Container>
+      {dummyData.map((item, firstTagIndex) => (
+        <div key={item.index}>
+          <FirstTag>{item.firstTag}</FirstTag>
+          <SecondTag>{item.secondTag}</SecondTag>
+          <ThirdTagContainer>
+            {item.thirdTag.map((thirdItem, thirdTagIndex) => (
+              <div key={thirdTagIndex}>
+                <MinusIcon onClick={() => removeThirdTag(firstTagIndex, thirdTagIndex)} /> {thirdItem}
+              </div>
+            ))}
+          </ThirdTagContainer>
+        </div>
+      ))}
+    </Container>
       );
     };
 
