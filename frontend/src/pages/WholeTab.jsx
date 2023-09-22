19244507676768f83a6b2188113b268a8918c7a4
박승희 <tael8213@gmail.com>
2023-09-22 17:38:08 +0900

    fix: git merge 오류 고침

--- frontend/src/pages/WholeTab.jsx
+++ frontend/src/pages/WholeTab.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import WholeTabContent from '../components/WholeTab/WholeTabContent';
 import Menubar from '../components/Common/Menubar';
->>>>>>> fd9bc7e24116d469a1bbfee57f656b79cd9c0e2e
 
 const WholeTab = () => {
     return (
         <div>
-<<<<<<< HEAD
-            
-=======
             <WholeTabContent />
             <Menubar />
->>>>>>> fd9bc7e24116d469a1bbfee57f656b79cd9c0e2e
         </div>
     );
 };
