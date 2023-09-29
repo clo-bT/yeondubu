719d88ec4503716ae79097b7bf08c7e80355b11a
hun-jae <tint1998@gmail.com>
2023-09-29 15:01:25 +0900

    fix: account 목록 조회시 계좌 타입 추가

--- backend/du-bu/src/main/java/yeon/dubu/account/dto/response/AccountInfoResDto.java
+++ backend/du-bu/src/main/java/yeon/dubu/account/dto/response/AccountInfoResDto.java
@@ -2,6 +2,7 @@ package yeon.dubu.account.dto.response;
 
 import lombok.Getter;
 import lombok.Setter;
+import yeon.dubu.account.enumeration.AccountType;
 
 @Getter
 @Setter
@@ -9,4 +10,5 @@ public class AccountInfoResDto {
     private Long id;
     private String name;
     private Long price;
+    private AccountType accountType;
 }
