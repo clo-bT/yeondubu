6c1ffb64d51baaa4fa2e45ff297617022bbfc64d
clo-bT <tael8213@gmail.com>
2023-10-03 02:35:17 +0900

    feat: calendar 새로

--- frontend/src/components/AccountCalendar/AccountCalendar.jsx
+++ frontend/src/components/AccountCalendar/AccountCalendar.jsx
@@ -1,167 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
 import { format, addMonths, subMonths } from 'date-fns';
 import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from 'date-fns';
 import { isSameMonth, isSameDay, addDays } from 'date-fns';
 import axios from 'axios';
 
-const Container = styled.div`
-`
-const CalendarHeader = styled.div`
-/* width: 355px; */
-display: flex;
-justify-content: space-between;
-/* padding: 10px; */
-font-size: 18px;
-font-weight: bold;
 
-`
-const Col = styled.div`
-flex: 1;
-padding: 5px;
-cursor: pointer;
-/* text-align: left; */
+const RenderHeader = ({ currentMonth, prevMonth, nextMonth }) => {
+    return (
+        <div className="header row">
+            <div className="col col-start">
+                <span className="text">
+                    
+                    {/* {format(currentMonth, 'yyyy')} */}
+                </span>
+            </div>
+            <div className="col">
+                <div onClick={prevMonth} />
+                <span className="text month">
+                        {format(currentMonth, 'M')}월
+                    </span>
+                <div onClick={nextMonth} />
+            </div>
+        </div>
+    );
+};
 
-`
-const TextMonth = styled.span`
-
-`
-// DaysClass는 요일 적힌 거
-const DaysClass = styled.div`
-/* width: 355px; */
-display: flex;
-justify-content: space-between;
-
-`
-
-const IncomeExpenditure = styled.div`
-flex-direction: column;
-
-`
-const CellClass = styled.div`
-// display: flex;
-// flex-direction: column;
-// align-items: center;
-// width: 50.71px;
-&.not-current-month {
-    color: rgba(0, 0, 0, 0.2); /* 전달 및 다음달 날짜의 텍스트 색상을 회색으로 지정 */
-}
-&.selected {    
-    width: 30px;
-    height: 30px;
-    background: rgba(255, 101, 101, 0.50);
-    border-radius: 50%;
-    z-index: 1;
-    // margin: 0 auto;
-}
-`
-const TextClass = styled.span`
-// display: flex;
-// flex-direction: row;
-// &.not-valid {
-//         color: rgba(0, 0, 0, 0.2); /* 전달 및 다음달 날짜의 텍스트 색상을 회색으로 지정 */
-//     }
-`
-const Row = styled.span`
-padding-top: .5rem;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-height: 3rem;
-
-`
-const IncomeText = styled.div`
-// color: #2663FF; /* 수입 텍스트 색상 */
-// /* font-weight: bold; */
-// font-size: .1rem;
-`
-const ExpenditureText = styled.div`
-// color: #FF6565; /* 지출 텍스트 색상 */
-// /* font-weight: bold; */
-// font-size: .1rem;
-`
-const RowsBody = styled.div`
-// display: flex;
-// flex-direction: column;
-
-`
-const TodayDot = styled.div`
-    width: 30px;
-    height: 30px;
-    background-color: #FF6565; /* 원하는 색상으로 설정 */
-    border-radius: 50%;
-    z-index: 1;
-    margin: 0 auto; 
-    color: white;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-`
-
-const NotTodayDot = styled.div`
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    z-index: 1;
-    margin: 0 auto; 
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-`
-
-
-const today = new Date();
-
-const AccountCalendar = ({ onDateClick : handleClick }) => {
-    const [currentMonth, setCurrentMonth] = useState(new Date());
-    const [selectedDate, setSelectedDate] = useState(new Date());
+const RenderDays = () => {
     const days = [];
     const date = ['일', '월', '화', '수', '목', '금', '토'];
-        // console.log(currentMonth)
-        const requestData = format(currentMonth, 'yyyy-MM')
-        // console.log('currentYear',requestData)
-        
-         const [responseData, setResponseData] = useState([]);
-         useEffect(() => {
-             // 백틱으로 바꾸기
-             const accessToken = localStorage.getItem("token");
-             axios.get(`${process.env.REACT_APP_API_ROOT}/api/v1/money/${requestData}`,{
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                }
-             })
-             .then(response => {
-                console.log(response)
-                 setResponseData(response.data);
-             })
-             .catch(error => {
-                 console.error('Error fetching data:', error);
-             });
-         }, [requestData]);
+
     for (let i = 0; i < 7; i++) {
         days.push(
-            <div key={i}>
+            <div className="col" key={i}>
                 {date[i]}
             </div>,
         );
     }
+
+    return <div className="days row">{days}</div>;
+};
+
+
+const today = new Date();
+
+
+
+const RenderCells = ({ currentMonth, selectedDate, onDateClick }) => {
     const monthStart = startOfMonth(currentMonth);
     const monthEnd = endOfMonth(monthStart);
     const startDate = startOfWeek(monthStart);
     const endDate = endOfWeek(monthEnd);
+    const formatday = format(today, 'yyyy-MM');
 
-
-    const dataMap = responseData.reduce((map, item) => {
+    const [responsedata, setResponsedata] = useState([])
+    useEffect(() => {
+        const accessToken = localStorage.getItem('token');
+        axios.get(`${process.env.REACT_APP_API_ROOT}/api/v1/money/${formatday}`,
+        {
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+            }
+        }).then((response) => {
+            console.log(response.data);
+            setResponsedata(response.data)
+        }).catch((error) => {
+            console.error(error);
+        })
+    },[formatday])
+    
+    const dataMap = responsedata.reduce((map, item) => {
         map[item.date] = item;
         return map;
     }, {});
 
     const rows = [];
-    let dates = [];
+    let days = [];
     let day = startDate;
     let formattedDate = '';
 
@@ -173,6 +86,7 @@ const AccountCalendar = ({ onDateClick : handleClick }) => {
             const income = dataMap[formattedDate]?.income || 0;
             const expenditure = dataMap[formattedDate]?.expenditure || 0;
             const cellClass = `
+                col cell 
                 ${!isSameMonth(day, monthStart) ? 'disabled' : ''}
                 ${isSameDay(day, selectedDate) ? 'selected' : ''}
                 ${format(currentMonth, 'M') !== format(day, 'M') ? 'not-valid' : ''}
@@ -186,46 +100,49 @@ const AccountCalendar = ({ onDateClick : handleClick }) => {
                 ${expenditure > 0 ? 'expenditure-text' : ''}
             `;
     
-            dates.push(
-                <CellClass
+            days.push(
+                <div
                     className={cellClass}
                     key={formattedDate}
                     onClick={() => onDateClick(cloneDay)}
                 >
-
-                    <TextClass className={textClass}>
-                    {isToday ? (
-                            <TodayDot>
-                                {/* <div> */}
-                                {formattedDate.split('-')[2]}
-                                {/* </div> */}
-                            </TodayDot> // 동그란 원 추가
-                        ) : 
-                            <NotTodayDot>
-
-                        <div>{formattedDate.split('-')[2]}</div>
-                            </NotTodayDot>
-                        
-                        }
+                    <span className={textClass}>
+                    {isToday && (
+                    <div className="todaydot">{formattedDate.split('-')[2]}</div> // 동그란 원 추가
+                        )}
+                    {!isToday && (
+                    <div>{formattedDate.split('-')[2]}</div>
                     
-                    </TextClass>
+                    )}
+                    </span>
 
-                    <IncomeExpenditure>
-                        <IncomeText>{income > 0 ? `+${income.toLocaleString()}` : ''}</IncomeText>
-                        <ExpenditureText>{expenditure > 0 ? `-${expenditure.toLocaleString()}` : ''}</ExpenditureText>
-                    </IncomeExpenditure>
+                    <div className={`income-expenditure ${income > 0 ? 'income' : expenditure > 0 ? 'expenditure' : ''}`}>
+                        <div className='incometext'>{income > 0 ? `+ ${income.toLocaleString()}원` : ''}</div>
+                        <div className='expendituretext'>{expenditure > 0 ? `- ${expenditure.toLocaleString()}원` : ''}</div>
+                    </div>
 
-                </CellClass>,
+                </div>,
             );
             day = addDays(day, 1);
         }
         rows.push(
-            <Row key={formattedDate}>
-                {dates}
-            </Row>,
+            <div className="row" key={formattedDate}>
+                {days}
+            </div>,
         );
-        dates = [];
+        days = [];
     }
+    return <div className="body">{rows}</div>;
+};
+
+
+
+
+
+const AccountCalendar = () => {
+    const [currentMonth, setCurrentMonth] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
     const prevMonth = () => {
         setCurrentMonth(subMonths(currentMonth, 1));
     };
@@ -234,31 +151,28 @@ const AccountCalendar = ({ onDateClick : handleClick }) => {
     };
     const onDateClick = (day) => {
         // console.log(day)
+        const formatday = format(day, 'yyyy-MM');
+        console.log(formatday)
         setSelectedDate(day);
-        const formatday = format(day, 'yyyy-MM-dd');
-        // console.log(formatday);
-        handleClick(formatday);
     };
-
+    const handleModalOpen = ()  => {
         
+    };
     return (
-        <Container>
-            <CalendarHeader>
-                <Col>
-                    <span onClick={prevMonth}>←</span>
-                    <TextMonth>
-                        {format(currentMonth, 'M')}월
-                    </TextMonth>
-                    <span onClick={nextMonth}>→</span>
-                </Col>
-            </CalendarHeader>
-            <DaysClass>
-                {days}
-            </DaysClass>
-            <RowsBody>
-                {rows}
-            </RowsBody>
-        </Container>
+        <div className="calendar">
+            <RenderHeader
+                currentMonth={currentMonth}
+                prevMonth={prevMonth}
+                nextMonth={nextMonth}
+            />
+            <RenderDays />
+            <RenderCells
+                currentMonth={currentMonth}
+                selectedDate={selectedDate}
+                onDateClick={onDateClick}
+            />
+            <div onClick={handleModalOpen}>추가하기</div>
+        </div>
     );
 };
 
