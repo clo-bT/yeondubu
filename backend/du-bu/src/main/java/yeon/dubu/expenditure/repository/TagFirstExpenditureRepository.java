6183e9aaae22f99c6b571448a1ab9ed3dc6f52a4
ysparrk <youngseo0703@gmail.com>
2023-10-02 22:01:46 +0900

    feat: 커플 삭제 시 관련 expenditureTag 삭제기능 추가

--- backend/du-bu/src/main/java/yeon/dubu/expenditure/repository/TagFirstExpenditureRepository.java
+++ backend/du-bu/src/main/java/yeon/dubu/expenditure/repository/TagFirstExpenditureRepository.java
@@ -3,6 +3,8 @@ package yeon.dubu.expenditure.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yeon.dubu.expenditure.domain.TagFirstExpenditure;
 
-public interface TagFirstExpenditureRepository extends JpaRepository<TagFirstExpenditure, Long>, CustomTagExpenditureRepository {
+import java.util.List;
 
+public interface TagFirstExpenditureRepository extends JpaRepository<TagFirstExpenditure, Long>, CustomTagExpenditureRepository {
+    List<TagFirstExpenditure> findByCoupleId(Long coupleId);
 }
