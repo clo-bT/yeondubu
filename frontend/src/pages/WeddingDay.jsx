fbd224bf990b24a82c0f2d528fc1db9b13263384
seongwonlee <jasmine1714@naver.com>
2023-10-03 19:37:26 +0900

    feat: 계좌 상세 api 연결

--- frontend/src/pages/WeddingDay.jsx
+++ frontend/src/pages/WeddingDay.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import WeddingDayInput from '../components/WeddingDay/WeddingDayInput';
+import CoupleImage from '../components/Common/CoupleImage';
 
 const WeddingDay = () => {
     return (
         <div>
+            <CoupleImage />
             <WeddingDayInput />
         </div>
     );
