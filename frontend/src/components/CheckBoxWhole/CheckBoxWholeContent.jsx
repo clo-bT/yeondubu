18cf8b402ff81e74b28b6756018c3cf716ca7b48
clo-bT <tael8213@gmail.com>
2023-10-05 06:51:39 +0900

    feat: 태그

--- frontend/src/components/CheckBoxWhole/CheckBoxWholeContent.jsx
+++ frontend/src/components/CheckBoxWhole/CheckBoxWholeContent.jsx
@@ -11,6 +11,8 @@ const Container = styled.div`
   justify-content: center;
   margin-top: 30px;
 `
+const Income = styled.div``
+const Expend = styled.div``
 const Box = styled.div`
 display: flex;
 justify-content: flex-end;
@@ -48,7 +50,7 @@ gap:5px;
 `
 const TagBox = styled.div`
 border-radius: 10px;
-border: 2px solid #FFA8A8;
+border: 2px solid #2663FF;
 background: rgba(255, 255, 255, 0.50);
 display: flex;
 width: 83px;
@@ -177,18 +179,12 @@ const CheckBoxWholeContent = () => {
         },[tagId])
           .then(response => {
             console.log('새로운 태그 추가', response);
-            console.log('새로운 태그 추가 firstName', firstName);
-            console.log('새로운 태그 추가v newTagName', newTagName);
-            console.log('새로운 태그 추가v tagId', tagId);
             setIsAddingTag(false);
             setNewTagName('');
             window.location.reload();
           })
           .catch(error => {
             console.error('새로운 태그 추가', error);
-            console.log('새로운 태그 추가 firstName', firstName);
-            console.log('새로운 태그 추가v newTagName', newTagName);
-            console.log('새로운 태그 추가v tagId', tagId);
 
           });
       } else if (isSelected === 2) {
@@ -209,14 +205,8 @@ const CheckBoxWholeContent = () => {
           })
           .catch(error => {
             console.error('새로운 태그 추가', error);
-            console.log('새로운 태그 추가 firstName', firstName);
-            console.log('새로운 태그 추가 secondName', secondName);
-            console.log('새로운 태그 추가 newTagName', newTagName);
           });
-
       }
-    
-      
     };
   const handleCheckboxChange = async (data) => {
     try {
@@ -254,94 +244,99 @@ const CheckBoxWholeContent = () => {
   };
   
     return (
+
         <div>
-            <Box>
-            <AddButton onClick={handleAddTag}>추가</AddButton>
-            {!isEditMode && <EditButton onClick={()=>setIsEditMode(true)}>편집</EditButton>}
-            {isEditMode && <EditButton onClick={()=>setIsEditMode(false)}>저장</EditButton>}
-            </Box>
-        <Container>
-        {(isSelected===0 )&& data.map((firstTag) => (
-          <TagBox key={firstTag.first_tag_id} onClick={() => handleSelect(firstTag.tag_second_expenditure_dto_list, firstTag.first_tag_name,firstTag.first_tag_id)}>
-                {isEditMode && (
-        <DeleteButton onClick={(e) => handleDeleteFirstTag(e, firstTag.first_tag_id)}>X</DeleteButton>
-      )}
-          <FirstTag>{firstTag.first_tag_name}</FirstTag>
-          <div>
-            {firstTag.tag_second_expenditure_dto_list.map((secondTag) => (
-              <SecondTag key={secondTag.second_tag_id}>
-                {secondTag.second_tag_name}
-                {/*
-                {secondTag.tag_third_expenditure_dto_list.map((thirdTag) => (
-                  <SecondTagItem key={thirdTag.third_tag_id}>
-                    {thirdTag.third_tag_name}
-                  </SecondTagItem>
-                ))}
-                */}
-              </SecondTag>
-            ))}
-          </div>
-        </TagBox>
-      ))}
-      {(isSelected===1) && (
-        <div>
-          <FirstTag onClick={()=>setIsSelected(0)}>{selectedName}</FirstTag>
-        {selectedData.map((data) => (
-        <TagBox key={data.second_tag_id} onClick={() => handleClick(data.tag_third_expenditure_dto_list,data.second_tag_name,data.second_tag_id)}>
-          {isEditMode && (
-        <DeleteButton onClick={(e) => handleDeleteFirstTag(e, data.second_tag_id)}>X</DeleteButton>
-      )}
-          <FirstTag>{data.second_tag_name}</FirstTag>
-          <Tag>
-            {data.tag_third_expenditure_dto_list.map((thirdTag) => (
-              <SecondTag key={thirdTag.second_tag_id}>
+         
+        <Expend><Box>
+
+        <AddButton onClick={handleAddTag}>추가</AddButton>
+        {!isEditMode && <EditButton onClick={()=>setIsEditMode(true)}>편집</EditButton>}
+        {isEditMode && <EditButton onClick={()=>setIsEditMode(false)}>저장</EditButton>}
+        </Box>
+    <Container>
+    {(isSelected===0 )&& data.map((firstTag) => (
+      <TagBox key={firstTag.first_tag_id} onClick={() => handleSelect(firstTag.tag_second_expenditure_dto_list, firstTag.first_tag_name,firstTag.first_tag_id)}>
+            {isEditMode && (
+    <DeleteButton onClick={(e) => handleDeleteFirstTag(e, firstTag.first_tag_id)}>X</DeleteButton>
+  )}
+      <FirstTag>{firstTag.first_tag_name}</FirstTag>
+      <div>
+        {firstTag.tag_second_expenditure_dto_list.map((secondTag) => (
+          <SecondTag key={secondTag.second_tag_id}>
+            {secondTag.second_tag_name}
+            {/*
+            {secondTag.tag_third_expenditure_dto_list.map((thirdTag) => (
+              <SecondTagItem key={thirdTag.third_tag_id}>
                 {thirdTag.third_tag_name}
-              </SecondTag>
+              </SecondTagItem>
             ))}
-          </Tag>
-        </TagBox>
+            */}
+          </SecondTag>
         ))}
-        </div>
-      )}
-      {(isSelected===2) && (
-        <div>
-          <FirstTag onClick={()=>setIsSelected(0)}>{selectedName}</FirstTag>
-        {selectedData.map((data) => (
-          <div key={data.third_tag_id} onClick={()=>navigate(`/calendarupdate/${data.third_tag_id}`)}>
-            <TagList>
-              <FirstTag>{data.third_tag_name}</FirstTag>
-              <FirstTag>{data.amount === 0 ? '예산 입력' : data.amount}</FirstTag>
-              <CheckBox
-                type="checkbox"
-                checked={data.pay_complete}
-                onChange={() => {
-                  if (data.amount !== 0 && data.user_role !== undefined) {
-                    handleCheckboxChange(data);
-                  }
-                }}
-                disabled={data.amount === 0 || data.user_role === undefined}
-              />
-            </TagList>
-        </div>
+      </div>
+    </TagBox>
+  ))}
+  {(isSelected===1) && (
+    <div>
+      <FirstTag onClick={()=>setIsSelected(0)}>{selectedName}</FirstTag>
+    {selectedData.map((data) => (
+    <TagBox key={data.second_tag_id} onClick={() => handleClick(data.tag_third_expenditure_dto_list,data.second_tag_name,data.second_tag_id)}>
+      {isEditMode && (
+    <DeleteButton onClick={(e) => handleDeleteFirstTag(e, data.second_tag_id)}>X</DeleteButton>
+  )}
+      <FirstTag>{data.second_tag_name}</FirstTag>
+      <Tag>
+        {data.tag_third_expenditure_dto_list.map((thirdTag) => (
+          <SecondTag key={thirdTag.second_tag_id}>
+            {thirdTag.third_tag_name}
+          </SecondTag>
         ))}
-        </div>
-      )}
-      {isAddingTag && (
-      <TagBox>
-        <NewTag
-          type="text"
-          value={newTagName}
-          onChange={(e) => setNewTagName(e.target.value)}
-          placeholder=""
-          ref={inputRef}
-        />
-        <Tag>
-        <FirstTag onClick={handleCancelClick}>취소</FirstTag>
-        <FirstTag onClick={handleSaveClick}>저장</FirstTag>
-        </Tag>
-      </TagBox>
-        )}
-        </Container>
+      </Tag>
+    </TagBox>
+    ))}
+    </div>
+  )}
+  {(isSelected===2) && (
+    <div>
+      <FirstTag onClick={()=>setIsSelected(0)}>{selectedName}</FirstTag>
+    {selectedData.map((data) => (
+      <div key={data.third_tag_id} onClick={()=>navigate(`/calendarupdate/${data.third_tag_id}`)}>
+        <TagList>
+          <FirstTag>{data.third_tag_name}</FirstTag>
+          <FirstTag>{data.amount === 0 ? '예산 입력' : data.amount}</FirstTag>
+          <CheckBox
+            type="checkbox"
+            checked={data.pay_complete}
+            onChange={() => {
+              if (data.amount !== 0 && data.user_role !== undefined) {
+                handleCheckboxChange(data);
+              }
+            }}
+            disabled={data.amount === 0 || data.user_role === undefined}
+          />
+        </TagList>
+    </div>
+    ))}
+    </div>
+  )}
+  {isAddingTag && (
+  <TagBox>
+    <NewTag
+      type="text"
+      value={newTagName}
+      onChange={(e) => setNewTagName(e.target.value)}
+      placeholder=""
+      ref={inputRef}
+    />
+    <Tag>
+    <FirstTag onClick={handleCancelClick}>취소</FirstTag>
+    <FirstTag onClick={handleSaveClick}>저장</FirstTag>
+    </Tag>
+  </TagBox>
+    )}
+    </Container></Expend>
+        
+            
 
     </div>
         );
