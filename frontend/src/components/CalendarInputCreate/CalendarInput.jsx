031f28e19eed64db1a106921b5c0dfbe9566e491
clo-bT <tael8213@gmail.com>
2023-10-05 02:18:24 +0900

    feat: 수입 지출 등록 연결

--- frontend/src/components/CalendarInputCreate/CalendarInput.jsx
+++ frontend/src/components/CalendarInputCreate/CalendarInput.jsx
@@ -1,120 +1,111 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
 
-`
 const InputType = styled.div`
-display: flex;
-align-items: center;
-`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
 const InputTypeIncome = styled.button.attrs(props => ({
-    style: {
-        color: props.checked ? "#FF937D" : "#000", 
-        border: props.checked ? "2px solid #FF937D" : "2px solid #D9D9D9", 
-        fontWeight: props.checked ? "bold" : "normal", 
-    },
+  style: {
+    color: props.checked ? "#FF937D" : "#000",
+    border: props.checked ? "2px solid #FF937D" : "2px solid #D9D9D9",
+    fontWeight: props.checked ? "bold" : "normal",
+  },
 }))`
-    height: 38px;
-    padding: 7px 15px;
-    gap: 10px;
-    border-radius: 10px;
-    background: #FFF;
-    cursor: pointer;
-`
+  height: 38px;
+  padding: 7px 15px;
+  border-radius: 10px;
+  background: #FFF;
+  cursor: pointer;
+`;
+
 const InputTypeExpend = styled.button.attrs(props => ({
-    style: {
-        color: props.checked ? "#FF937D" : "#000", 
-        border: props.checked ? "2px solid #FF937D" : "2px solid #D9D9D9", 
-        fontWeight: props.checked ? "bold" : "normal", 
-    },
+  style: {
+    color: props.checked ? "#FF937D" : "#000",
+    border: props.checked ? "2px solid #FF937D" : "2px solid #D9D9D9",
+    fontWeight: props.checked ? "bold" : "normal",
+  },
 }))`
-    height: 38px;
-    padding: 7px 15px;
-    gap: 10px;
-    border-radius: 10px;
-    background: #FFF;
-    cursor: pointer;
-    `
+  height: 38px;
+  padding: 7px 15px;
+  border-radius: 10px;
+  background: #FFF;
+  cursor: pointer;
+`;
 
 const InputWho = styled.div`
-display: flex;
-align-items: center;
-`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
 const InputWhoGroom = styled.button.attrs(props => ({
-    style: {
-        color: props.checked ? "#4F91F4" : "#000", 
-        border: props.checked ? "2px solid #4F91F4" : "2px solid #D9D9D9", 
-        fontWeight: props.checked ? "bold" : "normal", 
-    },
+  style: {
+    color: props.checked ? "#4F91F4" : "#000",
+    border: props.checked ? "2px solid #4F91F4" : "2px solid #D9D9D9",
+    fontWeight: props.checked ? "bold" : "normal",
+  },
 }))`
-    width: 87px;
-    height: 38px;
-    padding: 6px 4px;
-    gap: 10px;
-    border-radius: 10px;
-    background: #FFF;
-    cursor: pointer;
+  width: 87px;
+  height: 38px;
+  padding: 6px 4px;
+  border-radius: 10px;
+  background: #FFF;
+  cursor: pointer;
+`;
 
-`
 const InputWhoBride = styled.button.attrs(props => ({
-    style: {
-        color: props.checked ? "#FF937D" : "#000", 
-        border: props.checked ? "2px solid #FF937D" : "2px solid #D9D9D9", 
-        fontWeight: props.checked ? "bold" : "normal", 
-    },
+  style: {
+    color: props.checked ? "#FF937D" : "#000",
+    border: props.checked ? "2px solid #FF937D" : "2px solid #D9D9D9",
+    fontWeight: props.checked ? "bold" : "normal",
+  },
 }))`
-
-    width: 87px;
-    height: 38px;
-    padding: 6px 4px;
-    gap: 10px;
-    border-radius: 10px;
-    background: #FFF;
-    cursor: pointer;
-
-`
-const InputSpan = styled.span`
-margin-right: 30px;
-`
-const InputMemo = styled.div`
-display: flex;
-align-items: center;
-`
-const InputTag = styled.div`
-display: flex;
-align-items: center;
-`
-const InputDate = styled.div`
-display: flex;
-align-items: center;
-`
+  width: 87px;
+  height: 38px;
+  padding: 6px 4px;
+  border-radius: 10px;
+  background: #FFF;
+  cursor: pointer;
+`;
+const InputTag = styled.div``
+const InputDate = styled.div``
+const InputMoney = styled.div``
+const InputMemo = styled.div``
 const DateInput = styled.input``
 const MoneyInput = styled.input``
 const MemoInput = styled.input``
 const TagButton = styled.button`
+  width: 87px;
+  height: 38px;
+  padding: 6px 4px;
+  border-radius: 10px;
+  background: #FFF;
+  cursor: pointer;
+  border: 2px solid #D9D9D9;
+`;
 
-width: 87px;
-height: 38px;
-padding: 6px 4px;
-gap: 10px;
-border-radius: 10px;
-background: #FFF;
-cursor: pointer;
-border: 2px solid #D9D9D9;
-`
+const SaveButton = styled.button`
+  cursor: pointer;
+`;
 
-const InputMoney = styled.div`
-display: flex;
-align-items: center;
-`
-
-
-const SaveButton = styled.div`
-cursor: pointer;`
+const CompleteButton = styled.button`
+  cursor: pointer;
+`;
 
 const CalendarInput = () => {
+    const navigate = useNavigate();
     const accessToken = localStorage.getItem('token');
     //const role = localStorage.getItem('role');
     const [role, setRole] = useState('')
@@ -151,6 +142,7 @@ const CalendarInput = () => {
             "date": date,
             "amount":amount,
             "memo": memo,
+            
     },
         {
           headers: {
@@ -186,59 +178,114 @@ const CalendarInput = () => {
         };
         
         };
+    const handleComplete = () => {
+        if (type === 'income') {
+            axios.post(`${process.env.REACT_APP_API_ROOT}/api/v1/income`,
+        {
+            "tag_id":4,
+            "user_role": role,
+            "date": date,
+            "amount":amount,
+            "memo": memo,
+
+    },
+        {
+          headers: {
+              Authorization: `Bearer ${accessToken}`,
+            }
+        }).then(response => {
+            console.log('여기는 캘린더에서 수입 추가하기',response)
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        },[]);
+        }
+        else if (type === 'expenditure') {
+            axios.post(`${process.env.REACT_APP_API_ROOT}/api/v1/expenditure/money`,
+        {
+            "third_tag_id":1,
+            "user_role": role,
+            "date": date,
+            "amount":amount,
+            "memo": memo,
+            "pay_complete": true
+    },
+        {
+          headers: {
+              Authorization: `Bearer ${accessToken}`,
+            }
+        }).then(response => {
+            console.log('여기는 캘린더에서 지출 추가하기',response)
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        },[type]);
+        };
+        
+        };
     
-    return (
-        <Container>
-             <InputType>
-                        <InputSpan>분류 </InputSpan>
-                        <InputTypeIncome
-                            checked={type === 'income'}
-                            onClick={() => handleType('income')}
-                        >
-                            수입
-                        </InputTypeIncome>
-                        <InputTypeExpend
-                            checked={type === 'expenditure'}
-                            onClick={() => handleType('expenditure')}
-                        >
-                            지출
-                        </InputTypeExpend>
-                    </InputType>
-                    
-                    <InputWho><InputSpan>결제 </InputSpan>
-                        <InputWhoGroom
-                            checked={role === 'GROOM'}
-                            onClick={() => handleRole('GROOM')}
-                        >
-                            예비 신랑
-                        </InputWhoGroom>
-                        <InputWhoBride
-                            checked={role === 'BRIDE'}
-                            onClick={() => handleRole('BRIDE')}
-                        >
-                            예비 신부
-                        </InputWhoBride>
-                    </InputWho>
-                    <InputTag>
-                        <InputSpan>태그 </InputSpan>
-                        <TagButton value={tagId} onChange = {handleTagId}>태그</TagButton>
-                    </InputTag>
-                    <InputDate>
-                        <InputSpan>날짜 </InputSpan> 
-                        <DateInput type="date" value={date} onChange={handleDate} />
-                    </InputDate>
-                    <InputMoney>
-                        <InputSpan>가격 </InputSpan> 
-                        <MoneyInput type="number" value={amount} onChange={handleAmount} />원
-                    </InputMoney>
-                    <InputMemo>
-                        <InputSpan>메모 </InputSpan>
-                        <MemoInput type="text" value={memo} onChange={handleMemo} />
-                    </InputMemo>
-               
-            <SaveButton onClick={handleSave}>저장</SaveButton>
-        </Container>
-    );
+        return (
+            <Container>
+              <InputType>
+              분류
+                <InputTypeIncome
+                  checked={type === 'income'}
+                  onClick={() => handleType('income')}
+                >
+                  수입
+                </InputTypeIncome>
+                <InputTypeExpend
+                  checked={type === 'expenditure'}
+                  onClick={() => handleType('expenditure')}
+                >
+                  지출
+                </InputTypeExpend>
+              </InputType>
+        
+              <InputWho>
+              결제
+                <InputWhoGroom
+                  checked={role === 'GROOM'}
+                  onClick={() => handleRole('GROOM')}
+                >
+                  예비 신랑
+                </InputWhoGroom>
+                <InputWhoBride
+                  checked={role === 'BRIDE'}
+                  onClick={() => handleRole('BRIDE')}
+                >
+                  예비 신부
+                </InputWhoBride>
+              </InputWho>
+        
+              <InputTag>
+              태그
+                <TagButton value={tagId} onChange={handleTagId}>
+                  태그
+                </TagButton>
+              </InputTag>
+        
+              <InputDate>날짜
+                <DateInput type="date" value={date} onChange={handleDate} />
+              </InputDate>
+        
+              <InputMoney>가격
+                <MoneyInput type="number" value={amount} onChange={handleAmount} />원
+              </InputMoney>
+        
+              <InputMemo>메모
+                <MemoInput type="text" value={memo} onChange={handleMemo} />
+              </InputMemo>
+        
+              <div>
+                <SaveButton onClick={()=> navigate('/calendar')}>삭제</SaveButton>
+                <SaveButton onClick={handleSave}>저장</SaveButton>
+                {type === 'expenditure' && (
+                  <CompleteButton onClick={handleComplete}>구매완료</CompleteButton>
+                )}
+              </div>
+            </Container>
+          );
 };
 
 export default CalendarInput;
