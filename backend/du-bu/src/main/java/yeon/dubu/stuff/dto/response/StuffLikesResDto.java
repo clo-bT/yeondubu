1eb853d020ba09417dbaca98f3c90ac3598bbf88
ysparrk <youngseo0703@gmail.com>
2023-10-05 17:06:49 +0900

    refactor: stufflikes에 item_id 추가

--- backend/du-bu/src/main/java/yeon/dubu/stuff/dto/response/StuffLikesResDto.java
+++ backend/du-bu/src/main/java/yeon/dubu/stuff/dto/response/StuffLikesResDto.java
@@ -10,13 +10,15 @@ import lombok.*;
 public class StuffLikesResDto {
     private Long stuffId;
     private Long likesId;
+    private Long itemId;
     private String category;
     private String subCategory;
 
     @QueryProjection
-    public StuffLikesResDto(Long stuffId, Long likesId, String category, String subCategory) {
+    public StuffLikesResDto(Long stuffId, Long likesId, Long itemId, String category, String subCategory) {
         this.stuffId = stuffId;
         this.likesId = likesId;
+        this.itemId = itemId;
         this.category = category;
         this.subCategory = subCategory;
     }
