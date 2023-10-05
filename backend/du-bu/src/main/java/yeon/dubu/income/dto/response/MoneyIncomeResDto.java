fbdbbcccdccc5d9de8a8c15adb3a5483f262ec2e
ysparrk <youngseo0703@gmail.com>
2023-10-06 08:38:24 +0900

    feat: 수입 내역 조회 기능 구현

--- backend/du-bu/src/main/java/yeon/dubu/income/dto/response/MoneyIncomeResDto.java
+++ backend/du-bu/src/main/java/yeon/dubu/income/dto/response/MoneyIncomeResDto.java
@@ -1,10 +1,44 @@
 package yeon.dubu.income.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import yeon.dubu.income.domain.MoneyIncome;
+import yeon.dubu.user.enumeration.UserRole;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class MoneyIncomeResDto {
+    private Long incomeId;
+    private Long tagId;
+    private String tagName;
+    private LocalDate date;
+    private Long amount;
+    private String memo;
+    private UserRole userRole;
 
+    @Builder
+    public MoneyIncomeResDto(Long incomeId, Long tagId, String tagName, LocalDate date, Long amount, String memo, UserRole userRole){
+        this.incomeId = incomeId;
+        this.tagId = tagId;
+        this.tagName = tagName;
+        this.date = date;
+        this.amount = amount;
+        this.memo = memo;
+        this.userRole = userRole;
+    }
+
+    public static MoneyIncomeResDto from(MoneyIncome moneyIncome){
+        return MoneyIncomeResDto.builder()
+                .incomeId(moneyIncome.getId())
+                .tagId(moneyIncome.getTagIncome().getId())
+                .tagName(moneyIncome.getTagIncome().getTagName())
+                .date(moneyIncome.getDate())
+                .amount(moneyIncome.getAmount())
+                .memo(moneyIncome.getMemo())
+                .userRole(moneyIncome.getUserRole())
+                .build();
+    }
 }
