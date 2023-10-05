f81171508dfdcb9e9a98776896da40b1073eedc8
seongwonlee <jasmine1714@naver.com>
2023-10-05 12:33:13 +0900

    feat: css

--- frontend/src/pages/CheckBoxWhole.jsx
+++ frontend/src/pages/CheckBoxWhole.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CheckBoxHeader from '../components/CheckBoxWhole/CheckBoxHeader';
 import CheckBoxWholeContent from '../components/CheckBoxWhole/CheckBoxWholeContent';
+import Menubar from '../components/Common/Menubar';
 
 const CheckBoxWhole = () => {
     return (
         <div>
             <CheckBoxHeader/>
             <CheckBoxWholeContent/>
+            <Menubar/>
         </div>
     );
 };
