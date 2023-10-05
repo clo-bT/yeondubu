a36780b45cd3769af81ec04cf94c2d08423d96b7
felix0942364 <jaewoongcho93@gmail.com>
2023-10-06 08:21:44 +0900

    feat : css 추가

--- frontend/src/components/ShoppingRecommendation/RecommendationFilter.jsx
+++ frontend/src/components/ShoppingRecommendation/RecommendationFilter.jsx
@@ -1,5 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const Container = styled.div`
+overflow-y: auto;
+margin-left: 20px;
+margin-right: 20px;
+margin-top: 20px;
+margin-bottom: 110px;
+height: auto;
+`
 
 const Recommendation = styled.div`
 display: flex;
@@ -10,9 +22,12 @@ width: 100%;
 
 const RecommendationImg = styled.img`
 width: 100px;
+height: 100px; /* Set a fixed height of 100px */
+object-fit: cover; /* Crop the image if it exceeds the container */
 `
 
 const ImgDetail = styled.div`
+  width:31%;
   margin-right: 5px;
   display: flex;
   align-items: center; 
@@ -36,24 +51,120 @@ align-items: flex-start;
 justify-content: space-between;
 width: 100%;
 `
+
+const HeartIcon = styled(AiOutlineHeart)`
+  margin-left: 5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: red;
+`
+
+const FullHeartIcon = styled(AiFillHeart)`
+  margin-left: 5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: red;    
+`
+
+
 const RecommendationFilter = () => {
+    const accessToken = localStorage.getItem("token");
+    const {category, subcategory} = useParams();
     const [items, setItems] = useState([]);
+    const [likedArray, setLikedArray] = useState([]);
+    const [likedIdxs, setLikedIdxs] = useState([]);
+    const [likedItems, setLikedItems] = useState([]);
+    const [triggerUpdate, setTriggerUpdate] = useState(false);
+
     useEffect(() => {
         const getItem = localStorage.getItem('img_search');
         setItems(JSON.parse(getItem))
     }, []);
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_ROOT}/api/v1/marriage-stuffs/likes`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        })
+        .then((res) => {
+            let rawData = res.data;
+            let likedItemArray = rawData.filter((item) => item.category === category && item.subcategory === subcategory);
+            setLikedArray(likedItemArray);
+            let likeIndex = likedArray.map(item => item.item_id);
+            setLikedIdxs(likeIndex);
+            const ArrayToString = likeIndex.join(', ');
+            const params = {
+                category : category,
+                subcategory : subcategory,
+                likes : ArrayToString
+            };
+            axios.get(`${process.env.REACT_APP_FLASK_ROOT}/api/v1/marriage-stuffs/liked_items`, {params})
+            .then((response) => {
+                setLikedItems(response.data);
+                setTriggerUpdate(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }, [category, subcategory, accessToken, triggerUpdate]);
+
+    const likeItem = (itemID) => {
+        axios.post(`${process.env.REACT_APP_API_ROOT}/api/v1/marriage-stuffs/${category}/${subcategory}/${itemID}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        })
+        .then((res) => {
+            setTriggerUpdate(true);
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    };
+    
+    const unlikeItem = (itemID) => {
+        const like = likedArray.find((item) => item.item_id === itemID);
+        axios.delete(`${process.env.REACT_APP_API_ROOT}/api/v1/marriage-stuff/likes/${like.likes_id}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        })
+        .then((res) => {
+            setTriggerUpdate(true);
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    };
+
     console.log(items);
     return (
+      <Container>
         <Recommendation>
             {items.map((item, index) => (
               <ImgDetail key={index}>
-                <RecommendationImg src={item.image} />
+                <a href={item.link}>
+                  <RecommendationImg src={item.image} />
+                </a> 
                 <ImgDetailRow>
                   <Price>{item.lprice}</Price>
+                  {likedIdxs.includes(item.item_id) ? (
+                    <FullHeartIcon onClick = {() => unlikeItem(item.item_id)} />
+                  ) : (
+                    <HeartIcon onClick={() => likeItem(item.item_id)} />
+                  )}
                 </ImgDetailRow>
               </ImgDetail>
             ))}
         </Recommendation>
+      </Container>
     );
 };
 
