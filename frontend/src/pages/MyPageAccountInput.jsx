51a1d4840ce95c34a7932f15066199d1d7ee1f3d
seongwonlee <jasmine1714@naver.com>
2023-10-06 01:26:00 +0900

    feat: 마이페이지에 계좌 추가 폼 이동

---
+++ frontend/src/pages/MyPageAccountInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyPageAccountInputHeader from '../components/MyPageAccountInput/MypageAccountInputHeader';
+import MypageAccountInputForm from '../components/MyPageAccountInput/MypageAccountInputForm';
+
+const MyPageAccountInput = () => {
+    return (
+        <div>
+            <MyPageAccountInputHeader />
+            <MypageAccountInputForm />
+        </div>
+    );
+};
+
+export default MyPageAccountInput;
